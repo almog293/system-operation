7765d00cc0e4501c7cbc98ab8e26ae09b3efee82
almog293 <100486361+almog293@users.noreply.github.com>
2023-05-21 23:11:12 +0300

    Update message_slot.c

--- message_slot.c
+++ message_slot.c
@@ -8,7 +8,7 @@
 #include <linux/fs.h>       /* for register_chrdev */
 #include <linux/uaccess.h>  /* for get_user and put_user */
 #include <linux/string.h>   /* for memset. NOTE - not string.h!*/
-
+#include <linux/slab.h>   /* for kmalloc*/
 //Our custom definitions of IOCTL operations
 #include "message_slot.h"
 MODULE_LICENSE("GPL");
@@ -44,6 +44,7 @@ static long device_ioctl( struct   file* file,
                           unsigned long  ioctl_param )
 {
     int minor;
+    int i;
     channel *curr_ch,*curr_node,*last_node;
     if(ioctl_command_id != MSG_SLOT_CHANNEL)
     {
@@ -59,6 +60,7 @@ static long device_ioctl( struct   file* file,
     last_node = device_slot_list[minor].head;
     if(last_node == NULL) // minor is empty
     {
+    	printk("advisory - minor is empty\n");
         curr_ch = (channel *)kmalloc(sizeof(channel), GFP_KERNEL);
         if(curr_ch == NULL)
         {
@@ -67,22 +69,24 @@ static long device_ioctl( struct   file* file,
         }
         curr_ch->ch_id = ioctl_param;
         curr_ch->msg_size = 0;
-        curr_ch->message = NULL;
+        curr_ch->next = NULL;
         device_slot_list[minor].head = curr_ch;
     }
     else // minor is not empty
     {
+    	printk("advisory - minor is not empty\n");
         curr_node = device_slot_list[minor].head;
-        while (curr_node != NULL)
-        {
+        for(i = 0; i < 10; i++)
             //getting the channel with the ch_id
             //getting the last node of the linked list in the minor
             if(curr_node->ch_id == ioctl_param)
                 curr_ch = curr_node;
             last_node = curr_node;
             curr_node = curr_node->next;
+            if(curr_node == NULL)
+            	break;
         }
-        if(curr_node == NULL)
+        if(curr_ch == NULL)
         {
             //channel id does not exists for the minor
             curr_ch = (channel *)kmalloc(sizeof(channel), GFP_KERNEL);
@@ -93,13 +97,13 @@ static long device_ioctl( struct   file* file,
             }
             curr_ch->ch_id = ioctl_param;
             curr_ch->msg_size = 0;
-            curr_ch->message = NULL;
+            curr_ch->next = NULL;
             last_node->next = curr_ch;
         }
         
     }
     file->private_data = curr_ch;
-    printk("succesfull ioctl for minor %ld, channel %d\n",ioctl_param,minor);
+    printk("succesfull ioctl for minor %d, channel %ld\n",minor,ioctl_param);
 
 
     return SUCCESS;
@@ -114,9 +118,12 @@ static ssize_t device_read( struct file* file,
                             loff_t*      offset )
 {
     channel *curr_ch;
-    size_t i;
+    int count;
+    int minor;
+    char *temp_message_buff;
     printk( "Invocing device_read(%p,%ld)\n",file, length);
-
+    minor = iminor(file->f_inode);
+    printk("reading from minor %d\n",minor);
     if (buffer == NULL){
         printk("Error - reading buffer is NULL\n");
         return -EINVAL;
@@ -130,21 +137,28 @@ static ssize_t device_read( struct file* file,
     if(curr_ch->msg_size == 0)
     {
         printk("Error - no message to read\n");
+        printk("message size: %d, slot id: %d\n",curr_ch->msg_size,curr_ch->ch_id);
         return -EWOULDBLOCK;
     }
     if (curr_ch->msg_size > length){
         printk("Error - too long length to read\n");
         return -ENOSPC;
     }
-    for (i = 0; i < length; i++){
-        if (put_user(curr_ch->message[i], &buffer[i]) != 0)
-        {
-            printk("Error - put_user failed after %ld, reading out of %ld\n",i,length);
-            return -EFAULT;
-        }
+    temp_message_buff = kmalloc(sizeof(char) * curr_ch->msg_size, GFP_KERNEL);
+    if(temp_message_buff == NULL)
+    {
+    	printk("Error - fail kmalloc message buffer\n");
+    	return -ENOSPC;
     }
-    printk("succesfull %ld out of %ld bytes reading\n",i,length);
-    return SUCCESS;
+    memcpy(temp_message_buff,curr_ch->message,curr_ch->msg_size);
+    if((count = copy_to_user(buffer,temp_message_buff,curr_ch->msg_size)) != 0)
+    {
+    	printk("Error - copy to buffer \n");
+    	return -ENOSPC;
+    }
+    kfree(temp_message_buff);
+    printk("succesfull %d out of %d bytes reading\n",(curr_ch->msg_size - count),curr_ch->msg_size);
+    return (curr_ch->msg_size - count);
 }
 
 //---------------------------------------------------------------
@@ -155,9 +169,9 @@ static ssize_t device_write( struct file*       file,
                              size_t             length,
                              loff_t*            offset)
 {
-    size_t i,j;
+    int count;
     char temp[BUFFER_LEN];
-    channel curr_ch;
+    channel *curr_ch;
     printk("Invoking device_write(%p,%ld)\n", file, length);
     if (buffer == NULL){
         printk("Error - writing buffer is NULL\n");
@@ -167,23 +181,21 @@ static ssize_t device_write( struct file*       file,
         printk("Error - message length is 0 or more than 128\n");
         return -EMSGSIZE;
     }
-    curr_ch = (channel*)file->private_data;
+    curr_ch = (channel*)(file->private_data);
     if(curr_ch == NULL)
     {
         printk("Error - no channel to write from the minor\n");
         return -EINVAL;
     }
-    for (j = 0; j < length; j++){
-        if (get_user(temp[i], &buffer[i]) != 0){
-            printk("Error - get_user\n");
-            return -EFAULT;
-        }
+    if((count = copy_from_user(temp,buffer,length))!=0)
+    {
+    	printk("Error - copy\n");
+        return -ENOSPC;
     }
-    for(i=0; i < j; i++)
-        curr_ch.message[i] = temp[i];
-    curr_ch.msg_size = i;
-    printk("succesfull %ld out of %ld bytes writing\n",i,length);
-    return i;
+    memcpy(curr_ch->message,temp,length);
+    curr_ch->msg_size = length;
+    printk("succesfull %d out of %ld bytes writing\n",curr_ch->msg_size,length);
+    return curr_ch->msg_size;
 }
 
 //==================== DEVICE SETUP =============================
@@ -206,7 +218,7 @@ static int __init simple_init(void)
     int i;
     // Register driver capabilities. Obtain major num
     rc = register_chrdev( MAJOR_NUM, DEVICE_RANGE_NAME, &Fops );
-
+    printk("register %s, major number - %d\n",DEVICE_RANGE_NAME,MAJOR_NUM);
     // Negative values signify an error
     if( rc < 0 ) {
     printk( KERN_ALERT "%s registraion failed for  %d\n",DEVICE_RANGE_NAME, MAJOR_NUM );
